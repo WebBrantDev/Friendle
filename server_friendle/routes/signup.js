7de966608150587483ba003cfe216d13fa472497
Brant Smith <brant.lee.smith88@gmail.com>
2022-03-27 16:54:16 -0400

    Added outsideApiCalls file to improve separation of concerns.

--- server_friendle/routes/signup.js
+++ server_friendle/routes/signup.js
@@ -1,34 +1,8 @@
 const router = require("express").Router();
 const knex = require("knex")(require("../knex_db/knexfile").development);
 const bcrypt = require("bcryptjs");
-const axios = require("axios");
-const fs = require("fs");
 const validate = require("../utils/validators");
-
-// Generating a random seed for unique user icons
-const seedrandom = require("seedrandom");
-const generator = seedrandom();
-
-const userIconCall = (username) => {
-  const randomSeed = generator();
-  const filePath = `/assets/user_icons/${username}_icon.svg`;
-  axios
-    .get(`https://avatars.dicebear.com/api/identicon/${randomSeed}.svg`)
-    .then((res) => {
-      fs.writeFile(
-        `./assets/user_icons/${username}_icon.svg`,
-        res.data,
-        (err) => {
-          if (err) console.log(err);
-          console.log("It worked");
-        }
-      );
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return filePath;
-};
+const outsideApi = require("../utils/outsideApiCalls");
 
 router.post("/", (req, res) => {
   const data = req.body;
@@ -36,7 +10,7 @@ router.post("/", (req, res) => {
   const formattedUsername = username.replace(/ /g, "_");
   if (validate.emailCheck(email) && username) {
     const hashedPassword = bcrypt.hashSync(password, 10);
-    const avatar = userIconCall(formattedUsername);
+    const avatar = outsideApi.userIconCall(formattedUsername);
     console.log(avatar);
     knex("users")
       .insert({ avatar, username, email, password: hashedPassword })
--- server_friendle/utils/validators.js
+++ server_friendle/utils/validators.js
@@ -1,6 +1,4 @@
-const emailCheck = (email) => {
+exports.emailCheck = (email) => {
   const regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
   return regex.test(email);
 };
-
-exports.emailCheck = emailCheck;
