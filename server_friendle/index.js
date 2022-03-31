f3ecb7622b4bfe12143e79738dccef333837af15
Brant Smith <brant.lee.smith88@gmail.com>
2022-03-31 16:52:27 -0400

    Updates for deployment

--- server_friendle/index.js
+++ server_friendle/index.js
@@ -14,7 +14,7 @@ const testUsernameRoute = require("./routes/testUsername");
 const pullTeamDataRoute = require("./routes/pullTeamData");
 
 const app = express();
-const SERVER_PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -29,6 +29,6 @@ app.use("/joinTeam", joinTeamRoute);
 app.use("/testUsername", testUsernameRoute);
 app.use("/pullTeamData", pullTeamDataRoute);
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Listening on http://localhost:${SERVER_PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
